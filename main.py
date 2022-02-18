d1592a3a7c3605fc87bb2b53e7494f1acd600ce2
Jiarong Jin <98533787+jjin16@users.noreply.github.com>
2022-02-18 21:52:06 +0000

    added announcement for the race, changed the baground color and font size so the announcement can be seen more easiliy

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import turtle               #1. import modules
 import random
+import time
 
 #Part A
 window = turtle.Screen()       # 2.  Create a screen
 window.bgcolor('lightblue')
+window.colormode(255)
 
 michelangelo = turtle.Turtle()    # 3.  Create two turtles
 leonardo = turtle.Turtle()
@@ -20,19 +22,42 @@ leonardo.goto(-100,-20)
 ## 5. your code goes here
 ## method 1
 
-"""def turtle_go():
+def turtle_go():
   distance_to_move_for_mic= random.randrange(1,100)
   distance_to_move_for_leo= random.randrange(1,100)
   michelangelo.fd(distance_to_move_for_mic)
   leonardo.fd(distance_to_move_for_leo)
+def turtle_visibility(hidden_or_shown):
+    if hidden_or_shown == 1:
+        michelangelo.st()
+        leonardo.st()
+    else:
+        michelangelo.ht()
+        leonardo.ht()
 turtle_go()
+
 def reset_turtle():
   michelangelo.goto(-100,20)
   leonardo.goto(-100,-20)
-reset_turtle() 
-"""
+def winner_announce():
+    turtle_visibility(0)
+    if michelangelo.distance(-100, 20) > leonardo.distance(-100, -20):
+        michelangelo.write("Michelangelo wins! orange wins!",False ,"left" ,('Arial', 20, 'normal'))
+    elif michelangelo.distance(-100, 20) < leonardo.distance(-100, -20):
+        leonardo.write("Leonardo wins! blue wins!",False ,"left" ,('Arial', 20, 'normal'))
+    elif michelangelo.distance(-100, 20) == leonardo.distance(-100, -20):
+        michelangelo.write("draw! no one wins!",False ,"left" ,('Arial', 20, 'normal'))
+        leonardo.write("draw! no one wins!",False ,"left" ,('Arial', 20, 'normal'))
+    time.sleep(3)
+    turtle_visibility(1)
+    leonardo.clear()
+    michelangelo.clear()
+
+    
+winner_announce()    
+reset_turtle()
 ## method 2
-"""window.bgcolor("purple")
+window.bgcolor((236, 192, 250))
 def realistic_race():
   for call in range(10):
     mic_move= random.randrange(1,10)
@@ -40,8 +65,9 @@ def realistic_race():
     michelangelo.fd(mic_move)
     leonardo.fd(leo_move)
 realistic_race()
+winner_announce()
 reset_turtle()
-"""
+
 # Part B - complete part B here
 window.bgcolor("white")
 leonardo.pencolor("black")
@@ -50,7 +76,7 @@ def polygon(number_of_sides=1):
     side_length= 50
     inner_angle_per= inner_angle_tot/number_of_sides
     leonardo.down()
-    for i in range (number_of_sides):
+    for lines in range (number_of_sides):
       leonardo.fd(side_length)
       leonardo.rt(inner_angle_per)
     leonardo.clear()
@@ -64,6 +90,7 @@ def nonagon():
   polygon(9)
 def dodecagon():
   polygon(12)
+
 list_of_shapes= [eqtriangle(), square(), hexagon(), nonagon(), dodecagon()]
 for polygon_functions in list_of_shapes:
   polygon_functions
