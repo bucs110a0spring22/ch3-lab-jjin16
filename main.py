2b196337808a3386f9cac4b5fd7f889919620e56
Jiarong Jin <98533787+jjin16@users.noreply.github.com>
2022-02-18 20:14:17 +0000

    defined general function to make polygon. added functions to draw the shape required.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ leonardo.goto(-100,-20)
 ## 5. your code goes here
 ## method 1
 
-def turtle_go():
+"""def turtle_go():
   distance_to_move_for_mic= random.randrange(1,100)
   distance_to_move_for_leo= random.randrange(1,100)
   michelangelo.fd(distance_to_move_for_mic)
@@ -30,9 +30,9 @@ def reset_turtle():
   michelangelo.goto(-100,20)
   leonardo.goto(-100,-20)
 reset_turtle() 
-
+"""
 ## method 2
-window.bgcolor("purple")
+"""window.bgcolor("purple")
 def realistic_race():
   for call in range(10):
     mic_move= random.randrange(1,10)
@@ -40,7 +40,32 @@ def realistic_race():
     michelangelo.fd(mic_move)
     leonardo.fd(leo_move)
 realistic_race()
+reset_turtle()
+"""
 # Part B - complete part B here
+window.bgcolor("white")
+leonardo.pencolor("black")
+def polygon(number_of_sides=1):
+    inner_angle_tot= 360
+    side_length= 50
+    inner_angle_per= inner_angle_tot/number_of_sides
+    leonardo.down()
+    for i in range (number_of_sides):
+      leonardo.fd(side_length)
+      leonardo.rt(inner_angle_per)
+    leonardo.clear()
+def eqtriangle():
+  polygon(3)
+def square():
+  polygon(4)
+def hexagon():
+  polygon(6)
+def nonagon():
+  polygon(9)
+def dodecagon():
+  polygon(12)
+list_of_shapes= [eqtriangle(), square(), hexagon(), nonagon(), dodecagon()]
+for polygon_functions in list_of_shapes:
+  polygon_functions
 
-
-window.exitonclick()
+window.exitonclick
